29e10ab13d9f9ddc4fc7d485bd9a324dc02fbe12
Vlad <78665125+Bambalela@users.noreply.github.com>
2021-09-30 20:31:48 +0300

    FIX Добавлений рядок A0

--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ function returnSign($number): string
     }
 }
 
-function indexOfMinNumber($array, $isCol = true): int
+function indexOfMinNumber($array, bool $isCol ): int
 {
     $indexOfMin = -1;
     $minNumber = 0;
@@ -51,24 +51,40 @@ function indexOfMinNumber($array, $isCol = true): int
     return $indexOfMin;
 }
 
+function printSimplexTableOnes($table, $electedCol, $electedRow)
+{
+        echo "<table class='table'>";
+            echo "<thead>";
+                echo "<tr>";
+                    echo "<th scope='col'>Базис</th>";
+                    echo "<th scope='col'>C<sub>б</sub></th>";
+                    echo "<th scope='col'>A<sub>0</sub></th>";
+                    for($i=1;$i<count($table[$i]); $i++)
+                    {
+                        echo "<th scope='col'>x<sub>" . $i ."</sub></th>";
+                    }
+                echo "</tr>";
+            echo "</thead>";
+        echo "</$table>";
+}
+
 function simplexMethod($table, $resultRow, $cols, $rows){
-    $basisTable = [$rows+1][$cols];
+    $basisTable = [$rows+1][$cols+1];
     $electedRow = -1;
     $electedCol = -1;
+
+    for($i=0;$i<$rows;$i++) {$basisTable[$i][0] = $table[$i][count($table[$i])-1];}
     for($i=0;$i<$rows;$i++)
     {
-        for($j=0;$j<$cols-1;$j++)
-        {
-            $basisTable[$i][$j] = $table[$i][$j];
-        }
+        for($j=1;$j<=$cols-1;$j++) $basisTable[$i][$j] = $table[$i][$j-1];
     }
     // Заповнення рядку дельта Z
-    for($i=0;$i<=$cols;$i++){$basisTable[$rows][$i] = 0;}
-    for($i=0;$i<count($resultRow)-1;$i++){$basisTable[$rows][$i] = $resultRow[$i] * -1;}
+    for($i=0;$i<$cols;$i++){$basisTable[$rows][$i] = 0;}
+    for($i=1;$i<count($resultRow);$i++){$basisTable[$rows][$i] = $resultRow[$i-1] * -1;}
     // Заповння рядку О з косою рискою
     $electedCol = indexOfMinNumber($basisTable[$rows], true);
     $minColArr = $lastCol = [$rows];
-    for($i=0;$i<$rows;$i++) $minColArr[$i] = $basisTable[$i][$electedCol];
+    for($i=0;$i<=$rows;$i++) $minColArr[$i] = $basisTable[$i][$electedCol];
     for($i=0;$i<$rows;$i++) {
         $lastCol[$i] = $table[$i][count($table[$i])-1]/$minColArr[$i];
         if($lastCol[$i] < 0) $lastCol[$i] = 0;
@@ -78,17 +94,16 @@ function simplexMethod($table, $resultRow, $cols, $rows){
         $basisTable[$i][count($basisTable[$i])] = $lastCol[$i];
     }
     $electedRow = indexOfMinNumber($lastCol, false);
-    echo $electedRow;
 
         debugToConsole(" ");
-    debugToConsole("simplex method table:");
-    for ($i = 0; $i <= $rows; $i++) {
-        $debugRes = "";
-        for ($j = 0; $j < $cols; $j++) {
-            $debugRes .= ($basisTable[$i][$j] . " ");
+        debugToConsole("simplex method table:");
+        for ($i = 0; $i <= $rows; $i++) {
+            $debugRes = "";
+            for ($j = 0; $j <= $cols; $j++) {
+                $debugRes .= ($basisTable[$i][$j] . " ");
+            }
+            debugToConsole($debugRes);
         }
-        debugToConsole($debugRes);
-    }
 
 echo "<li>";
 
