78dcd30519875821e627e57cf02c6f85075a0720
vlad <yarysh.vlad@gmail.com>
2021-10-31 19:46:57 +0200

    All is working?!!!

--- symplexDoubled.php
+++ symplexDoubled.php
@@ -86,17 +86,85 @@ function printDoubledSimplex($electedRow, $electedCol, $ORow, $table, $basisInde
     echo "</table>";
 }
 
-function DoubledSimplex($electedCol, $electedRow, $ORow, $table){
+function DoubledSimplex($electedCol, $electedRow, $ORow, $table, $resultRow){
     $j = $_SESSION["functionCols"]; // заповнення базисних індексів
     for($i = 0; $i < count($table) - 1; $i++){
         $basisIndexes[0][$i] = $j + $i + 1;
     }
-
     for($i = 0; $i < count($basisIndexes[0]); $i++){
         $basisIndexes[1][$i] = 0;
     }
 
-    printDoubledSimplex($electedRow, $electedCol, $ORow, $table, $basisIndexes);
+    for($iteration = 1; true; $iteration++){
+        echo "<li>";
+            echo "<br><h4 style='margin-left: 50px'>\tІтерація #". $iteration ."</h4>";
+            printDoubledSimplex($electedRow, $electedCol, $ORow, $table, $basisIndexes);
+
+            //if($iteration == 2) break;
+
+            //перевірка на числа < 0
+            for($i = 0;$i < count($table) - 1; $i++)
+                $ACol[$i] = $table[$i][0];
+            $electedNumber = min($ACol);
+        debugToConsole($electedNumber);
+
+        if($electedNumber >= 0) break;
+            //Ставимо нові значення базису
+            $basisIndexes[0][$electedRow] = $electedCol;
+            $basisIndexes[1][$electedRow] = (($electedCol - 1 < (count($resultRow) - 1)? $resultRow[$electedCol - 1] : 0));
+
+            $rows = count($table);
+            $cols = count($table[0]);
+            //Перераховуємо симплекс таблицю
+            for($i = 0; $i < $rows; $i++){
+                for($j = 0; $j < $cols; $j++){
+                    $tempTable[$i][$j] = round(($table[$electedRow][$electedCol] * $table[$i][$j]
+                        - $table[$electedRow][$j] * $table[$i][$electedCol])
+                        /$table[$electedRow][$electedCol],1);
+                }
+            }
+
+                //Перераховуємо вибраний рядок
+            for($j = 0; $j < $cols; $j++){
+                $tempTable[$electedRow][$j] = round($table[$electedRow][$j] / $table[$electedRow][$electedCol],1);
+            }
+
+        debugToConsole('');
+        for($i = 0; $i < $rows; $i++){
+            $test = '';
+            for($j = 0; $j < $cols; $j++){
+                $test .=  $tempTable[$i][$j] . ' ';
+            }
+            debugToConsole($test);
+        }
+        debugToConsole('');
+
+
+        // Перешуковуємо рядок
+            for($i = 0;$i < $rows - 1; $i++) $tempArr[$i] = $tempTable[$i][0];
+            $electedNumber = min($tempArr);
+            for($i = 0;$i < $rows; $i++) if($tempArr[$i] == $electedNumber){ $electedRow = $i; break; }
+            debugToConsole("elected row " . $electedRow);
+
+                // Перераховуємо О з рискою
+            for($i = 1; $i < $cols; $i++){
+                try{
+                    if($tempTable[$rows - 1][$i] == 0) throw new Exception;
+                    $ORow[$i - 1] = -1 * round($tempTable[$rows - 1][$i]/$tempTable[$electedRow][$i] ,2);
+                } catch (Exception $e){
+                    $ORow[$i - 1] = PHP_INT_MAX;
+                }
+            }
+                // Перешукуємо стовбець
+            $electedNumber = min($ORow);
+            for($i = 0;$i < count($ORow); $i++) if($ORow[$i] == $electedNumber){ $electedCol = $i + 1; break; }
+            debugToConsole("elected col ".$electedCol);
+
+        echo "</li>";
+
+        $table = $tempTable;
+        //break;
+    }
 }
 
 function doubledSimplexMethodMain($table, $resultRow){
@@ -132,7 +200,7 @@ function doubledSimplexMethodMain($table, $resultRow){
     for($i = 1; $i < $cols - 1; $i++){
         try{
             if($simplexTable[$rows][$i] == 0) throw new Exception;
-        $ORow[$i - 1] = -$simplexTable[$rows][$i]/$simplexTable[$electedRow][$i];
+        $ORow[$i - 1] = -1 * $simplexTable[$rows][$i]/$simplexTable[$electedRow][$i];
             } catch (Exception $e){
             $ORow[$i - 1] = PHP_INT_MAX;
         }
@@ -141,7 +209,7 @@ function doubledSimplexMethodMain($table, $resultRow){
     for($i = 0;$i < count($ORow); $i++) if($ORow[$i] == $electedNumber){ $electedCol = $i + 1; break; }
     debugToConsole("elected col ".$electedCol);
 
-    DoubledSimplex($electedCol, $electedRow, $ORow, $simplexTable);
+    DoubledSimplex($electedCol, $electedRow, $ORow, $simplexTable, $resultRow);
 }
 
 function dealWithDoubled($table, $resultRow){
